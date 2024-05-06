d73c448d85bfea22dc3233589e4a2b6e065dc18c
mikezaech <98896982+mikezaech@users.noreply.github.com>
2024-05-06 11:29:20 +0200

    Update switchedPwlWaveform.m

--- functions/switchedPwlWaveform.m
+++ functions/switchedPwlWaveform.m
@@ -13,5 +13,21 @@ function [timestamp, switchedTrace]  = switchedPwlWaveform(trace,time,states)
     
     switchedTrace = statesDoubled.*traceDoubled;
     
-
+    % Detect Sign Change
+    signChangeIdx = find(abs(diff(sign(switchedTrace))) == 2); % The next value will have a different sign
+    if isempty(signChangeIdx) == 0
+        % Add zero crossing
+        for n = 1:length(signChangeIdx)
+            traceIdx = signChangeIdx(n);
+            t0(n) = -switchedTrace(traceIdx)/((switchedTrace(traceIdx+1) - switchedTrace(traceIdx))/(timestamp(traceIdx+1) - timestamp(traceIdx)));
+        end
+        % add 0 to trace
+        switchedTrace = [switchedTrace,zeros(1,length(t0))];
+        % sort timestamps with zero crossings
+        [timestamp, sortIdx] = sort([timestamp,t0]);
+        % sort traces:
+        switchedTrace = switchedTrace(sortIdx);
+    end
+    
+    
 end
